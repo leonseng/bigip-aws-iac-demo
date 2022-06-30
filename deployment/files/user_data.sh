483582eeb3c80eaa3c9ef57eda52df5ef1880d78
Leon Seng <w.leon.seng@gmail.com>
2022-06-30 10:51:42 +1000

    Updated workload VM to autostart docker on reboot

--- deployment/files/user_data.sh
+++ deployment/files/user_data.sh
@@ -3,5 +3,6 @@ yum update -y
 amazon-linux-extras install docker
 service docker start
 usermod -aG docker ec2-user
+systemctl enable docker
 
 docker run -d --name f5demo  --rm -p 80:80 f5devcentral/f5-demo-httpd:nginx
